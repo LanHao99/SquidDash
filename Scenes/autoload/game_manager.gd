6d9303a644d864d50b92a643c67afe1fb582f297
Gu Runjing <lan_hao@sjtu.edu.cn>
2025-12-21 13:13:35 +0800

    gear

---
+++ Scenes/autoload/game_manager.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var death_count = 0
+
+
---
+++ Scripts/gear.gd
@@ -0,0 +1,6 @@
+extends Area2D
+@onready var GearAnimation = $GearAnimation
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group(&"Player"):
+		pass
